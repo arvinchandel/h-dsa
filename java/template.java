ba09824872f10eb9cc5ccc6f3ae18ca20b884f15
Bhanu <arvinchandel66@gmail.com>
2026-07-13 18:42:52 +0530

    chore: add java template for problems

---
+++ java/template.java
@@ -0,0 +1,14 @@
+//source:hackerank
+// difficulty <easy|medium|hard>
+//tags: <arrays,strings,recursion>
+import java.io.*;
+import java.util.*;
+public class solution{
+public static void main(Sting [] args) throws exception{
+BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+String line =br.readLine();
+//parse input here
+//call solve and print result 
+}
+// helper methods 
+}
