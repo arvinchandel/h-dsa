450f0cca3bb604f11d6cb340d8f1bd8d54fc6957
Bhanu <arvinchandel66@gmail.com>
2026-07-13 19:14:08 +0530

    feat(arrays): add stdin-stdout-1 solution (hackerank)

---
+++ java/arrays/stdin.java
@@ -0,0 +1,23 @@
+// HackerRank: Java Stdin and Stdout I
+// Difficulty: Easy
+// Tags: io, basics
+import java.io.*;
+import java.util.*;
+
+public class stdin-stdout-i {
+        public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        int a,b,c;
+        a=sc.nextInt();
+        b=sc.nextInt();
+        c=sc.nextInt();
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        
+        
+        
+    }
+}
+
